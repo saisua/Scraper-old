e0f2b66f9a4653a8665a1f8a6d987acfbf6ce13c
saisua <ausias.prieto@gmail.com>
2019-01-26 11:57:07 +0100

    Working prototype

--- analyzer2.py
+++ analyzer2.py
@@ -1,52 +1,75 @@
+#Import
 import xml.etree.ElementTree as ET
 import threading
 import sys
+import itertools
 
+
+# Thread create and run with treading module
 class Search_thread(threading.Thread):
-    def __init__(self, search:str, search_in:dict, analizer):
+    # Init sets up the thread´s future work
+    def __init__(self, search:str, search_tags, search_in, analizer):
         threading.Thread.__init__(self)
         self.search = search
-        self.search_in = list(search_in.values())
-        self.search_name = list(search_in.keys())
+        self.tag_obj = list(itertools.chain.from_iterable([search_in.findall(tag) for tag in search_tags]))
+        self.search_tags = list(itertools.chain.from_iterable([list(find.attrib.values()) for find in self.tag_obj]))
         self.analizer = analizer
 
+    # Run gets executed by threading when the program runs 
+    # thread.start()
     def run(self):
-        for num in range(len(self.search_in)):
-            position = self.search_in[num].find(self.search)
-            print(position) #Delete debug
+        for search in range(len(self.search_tags)):
+            position = self.search_tags[search].find(self.search)
             result = []
             while (position >= 0):
                 result.append(position)
-                position = self.search_in[num].find(self.search,position+1)
-            self.analizer.add_to_result(self.search_name[num],result)
+                position = self.search_tags[search].find(self.search,position+1)
+            self.analizer.add_to_result(self.search,{self.tag_obj[search]:result})
 
+
+# Analizer class
 class Analizer(object):
-    def __init__(self, search:str, search_in:dict, threads:int=1):
-        print("Analizer initing")
-        if(len(search_in)<threads):
-            threads = len(search_in)
-        part_size = len(search_in)/threads
-        search_in_part = dict([search_in.items()[int(part_size*thread_num):int(part_size*(thread_num+1))] for thread_num in range(threads)])
-        threads_list = [Search_thread(search,search_in_part.items()[thread_num],self) for thread_num in range(threads)]
+    # Init Analizer object to create {threads} threads
+    # and analize the ET(from .xml) file given
+    def __init__(self, search:dict, search_in, threads:int=1):
         self.result = {}
-        print(f"Starting threads {threads_list}")
+        threads_list = []
+
+        # May need improvements and to fix the max threads
+        for searching in search.items():
+            threads_list.append(Search_thread(searching[0],searching[1], search_in,self))
+            
+        print(f"Starting {len(threads_list)} threads.") 
+        ended = 0
         for thread in threads_list: 
             thread.start()
             thread.join()
+            ended += 1
+            print(f"Ended: {ended}       ", end="\r")
+        print("All threads ended sucessfully")
 
+    # As the threads run in parallel they don't return
+    # the data as it would overwritte, so i execute this
+    # function and save the data asynclly
     def add_to_result(self, key:str, to_add):
-        self.result[key] = to_add
-        
+        if(not key in self.result.keys()): self.result[key] = []
+        self.result[key].append(to_add)   
+
+    # Returns the data analized
     def get_result(self):
         return self.result
+
 
 if __name__ == "__main__":
     file = ET.parse("c1.xml")
     root = file.getroot()
-    search_in = root.attrib
-    search_in_keys = ["link","text"]
+    search_in_keys = ["link","text"] #Debug
     if(len(search_in_keys)):
-        search_in = dict([(in_key, in_val) for (in_key, in_val) in root.])
-    #I need to think in a way to pass the key, value and obj
-
-    #print(Analizer("long", open("test.txt","r").read(), 1).get_result())
+        search_in = root #Search starting point
+        search = {"ins":["site","text"]} #Debug
+        f = Analizer(search, search_in, 8).get_result()
+        print(f)
+        print("[R]",end=" ")
+        print("\n[R] ".join([(list(c.attrib.values())[0][e-5:e+500]) for a in f.values() for b in a for c,d in b.items() for e in d]))
+        # This crappy line represents what the program is supposed to do
+        # It will later be a function
