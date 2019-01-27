675a9b68e69c263c9ae995017675c5be6a48c2f4
saisua <ausias.prieto@gmail.com>
2019-01-27 08:30:39 -0500

    Some bugs found that are being fixed

--- analyzer2.py
+++ analyzer2.py
@@ -1,4 +1,4 @@
-#Import
+5#Import
 import xml.etree.ElementTree as ET
 import threading
 import sys
@@ -8,23 +8,24 @@ import itertools
 # Thread create and run with treading module
 class Search_thread(threading.Thread):
     # Init sets up the thread´s future work
-    def __init__(self, search:str, search_tags, search_in, analizer):
+    def __init__(self, search_list:list, analizer:object):
         threading.Thread.__init__(self)
-        self.search = search
-        self.tag_obj = list(itertools.chain.from_iterable([search_in.findall(tag) for tag in search_tags]))
-        self.search_tags = list(itertools.chain.from_iterable([list(find.attrib.values()) for find in self.tag_obj]))
+        self.search_list = search_list
         self.analizer = analizer
 
     # Run gets executed by threading when the program runs 
     # thread.start()
     def run(self):
-        for search in range(len(self.search_tags)):
-            position = self.search_tags[search].find(self.search)
+        # search = [tag, search, text, obj]
+        for search in self.search_list:
+            if(search is None): break
+            position = search[2].find(search[1])
             result = []
             while (position >= 0):
                 result.append(position)
-                position = self.search_tags[search].find(self.search,position+1)
-            self.analizer.add_to_result(self.search,{self.tag_obj[search]:result})
+                position = search[2].find(search[1],position+1)
+            self.analizer.add_to_result(search[1],{search[3]:[search[2],result]})
+            # {search: {obj: [text, result]}}
 
 
 # Analizer class
@@ -36,19 +37,41 @@ class Analizer(object):
         threads_list = []
 
         # May need improvements and to fix the max threads
-        for searching in search.items():
-            threads_list.append(Search_thread(searching[0],searching[1], search_in,self))
+        for searching in self.format_data(search,search_in,threads):
+            threads_list.append(Search_thread(searching,analizer=self))
             
-        print(f"Starting {len(threads_list)} threads.") 
+        print(f"Starting {len(threads_list)} thread{'s' if len(threads_list) != 1 else ''}.") 
         ended = 0
-        for thread in threads_list: 
+
+        # Run threads
+        for thread in threads_list:
             thread.start()
             thread.join()
             ended += 1
-            print(f"Ended: {ended}       ", end="\r")
+            print(f"Ended: {ended}/{len(threads_list)}       ", end="\r")
         print("All threads ended sucessfully")
 
-    # As the threads run in parallel they don't return
+    # Basically finds all strings in tag&attr
+    # and returns the tag, the what's being searched,
+    # the string itself and the object containing it,
+    # splitting it in {threads} parts
+    def format_data(self, search:dict, search_in, threads:int):
+        fin = []
+        for (searching,search_dict) in search.items():
+            for (tag,attrs) in search_dict.items():
+                objs = search_in.findall(tag)
+                for obj in objs:
+                    if("*" in attrs):
+                        for value in obj.attrib.values():
+                            fin.append([tag, searching, value, obj])
+                        continue
+                    for attr in attrs:
+                        if(attr in obj.attrib.keys()):
+                            fin.append([tag, searching, obj.attrib[attr], obj])
+
+        return itertools.zip_longest(*[iter(fin)]*threads, fillvalue=None)
+
+    # As the threads run in parallel they don't returnlen(threads_list)
     # the data as it would overwritte, so i execute this
     # function and save the data asynclly
     def add_to_result(self, key:str, to_add):
@@ -56,20 +79,32 @@ class Analizer(object):
         self.result[key].append(to_add)   
 
     # Returns the data analized
-    def get_result(self):
+    def get_result_raw(self):
         return self.result
 
+    def get_result(self):
+        return 
+
+    # {search: {obj: [text, result]}}
+    def print_result(self, bef_margin=10, aft_margin=30):
+        for search, places in self.result.items():
+            print(f" - [{search}]")
+            num = 0
+            for place in places:
+                found = list(place.items())[0]
+                text, positions = found[1]
+                for pos in positions:
+                    print("".join([f"[{num}] {text[pos-bef_margin:pos+aft_margin]}"]))
+                    num += 1
 
 if __name__ == "__main__":
     file = ET.parse("c1.xml")
     root = file.getroot()
-    search_in_keys = ["link","text"] #Debug
-    if(len(search_in_keys)):
-        search_in = root #Search starting point
-        search = {"ins":["site","text"]} #Debug
-        f = Analizer(search, search_in, 8).get_result()
-        print(f)
-        print("[R]",end=" ")
-        print("\n[R] ".join([(list(c.attrib.values())[0][e-5:e+500]) for a in f.values() for b in a for c,d in b.items() for e in d]))
-        # This crappy line represents what the program is supposed to do
-        # It will later be a function
+    search_in = root #Search starting point
+    search = {"/static/images/":{"*":["*"]},"ins":{"site":["link","text"],"text":["link","text"]}} #Debug
+    f = Analizer(search, search_in, 8)
+    f.print_result()
+
+# Bugs
+# Doesn't find in "*"
+# Creates random threads
